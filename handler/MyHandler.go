39311e5601f289fee3ffcd957775edfa37aa2322
wangxuan <15038713379@163.com>
2023-08-14 17:57:44 +0800

    feat: 统一异常拦截处理

--- handler/MyHandler.go
+++ handler/MyHandler.go
@@ -1,13 +1,15 @@
 package handler
 
 import (
+	"ginProject/result"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"runtime/debug"
 )
 
-// HandlerToken gin中间件，类似Java拦截器AOP
-func HandlerToken() gin.HandlerFunc {
+// TokenHandler gin中间件，类似Java拦截器AOP
+func TokenHandler() gin.HandlerFunc {
 
 	return func(context *gin.Context) {
 		if auth := context.Request.Header.Get("Authorization"); auth == "nil" || auth == "" {
@@ -18,4 +20,23 @@ func HandlerToken() gin.HandlerFunc {
 		context.Next()
 		context.JSON(http.StatusOK, "授权成功")
 	}
+}
+
+// Recover 定义统一异常处理
+func Recover(context *gin.Context) {
+	defer func() {
+		if err := recover(); err != nil {
+			// 异常日志
+			log.Printf("出现异常: %v\n", err)
+			// 打印错误堆栈信息
+			debug.PrintStack()
+			// 返回统一的Json风格
+			//todo 判断异常类型
+			context.JSON(http.StatusOK, result.FAIL.BuildMsg(err.(string), context.Request.URL.String()))
+			//终止后续操作
+			context.Abort()
+		}
+	}()
+	//继续操作
+	context.Next()
 }
